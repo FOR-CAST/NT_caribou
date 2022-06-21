90738aab7642a0647dc376bc2f15b548126bd347
Alex Chubaty <achubaty@for-cast.ca>
2022-06-21 16:23:08 -0600

    update pkg install script

--- 01-packages.R
+++ 01-packages.R
@@ -1,9 +1,11 @@
 source("01a-packages-libPath.R")
 
-if (!require("Require", quietly = TRUE)) {
-  install.packages("Require")
-  library(Require)
+if (!"Require" %in% rownames(installed.packages())) {
+  remotes::install_github("PredictiveEcology/Require@development")
+} else if (packageVersion("Require") < "0.1.0.9000") {
+  remotes::install_github("PredictiveEcology/Require@development")
 }
+library(Require)
 
 .spatialPkgs <- c("lwgeom", "rgdal", "rgeos", "sf", "sp", "stars", "raster", "terra")
 
@@ -15,6 +17,14 @@ if (FALSE) {
   installSpatialPackages()
   #install.packages(c("raster", "terra"), repos = "https://rspatial.r-universe.dev")
   sf::sf_extSoftVersion() ## want at least GEOS 3.9.0, GDAL 3.2.1, PROJ 7.2.1
+}
+
+## WORKAROUND: missing CRAN packages + related errors
+if (FALSE) {
+  install.packages("https://cran.r-project.org/src/contrib/Archive/gdalUtils/gdalUtils_2.0.3.2.tar.gz", repos = NULL)
+  install.packages("https://cran.r-project.org/src/contrib/Archive/RandomFields/RandomFields_3.3.14.tar.gz", repos = NULL)
+  install.packages(c("foreach", "R.utils")) ## TODO: RandomFields ?
+  remotes::install_github("PredictiveEcology/LandR@development")
 }
 
 out <- makeSureAllPackagesInstalled(modulePath = "modules")
