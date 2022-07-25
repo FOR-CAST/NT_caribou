8248a9d08d1b119b239f71c57639c872b867af14
Alex Chubaty <achubaty@for-cast.ca>
2022-07-25 12:37:08 -0600

    update rsync script

--- scripts/rsync_outputs.sh
+++ scripts/rsync_outputs.sh
@@ -10,4 +10,4 @@ tar -vc --use-compress-program="pigz -p 16" -f posthoc.tar.gz posthoc
 popd
 
 rsync -avzP /mnt/projects/WBI/NT_caribou/outputs/*.tar.gz narval:/home/achubaty/projects/rrg-stevec/achubaty/NT_caribou/outputs/
-rsync -avzP /mnt/projects/WBI/NT_caribou/outputs/NT1/posthoc.tar.gz /mnt/wbi_data_orig/NT_caribou/outputs/
+rsync -avzP /mnt/projects/WBI/NT_caribou/outputs/NT1/posthoc.tar.gz narval:/home/achubaty/projects/rrg-stevec/achubaty/NT_caribou/outputs/
