01e7cc6a00d6a34f956e9efa278735587957994c
Tati Micheletti <tati.micheletti@gmail.com>
2022-07-29 04:04:48 -0600

    Change made to deal with permission's problem -- if solved, commit can be reversed

--- 03-paths.R
+++ 03-paths.R
@@ -8,7 +8,8 @@ defaultPaths <- list(
   cachePath = cacheDir,
   modulePath = "modules",
   inputPath = "inputs",
-  outputPath = file.path("outputs", studyAreaName),
+  # outputPath = file.path("outputs", studyAreaName),
+  outputPath = file.path("outputs/NT1"), # TM: Changed to be able to deal with permission's problem
   scratchPath = scratchDir
 )
 
